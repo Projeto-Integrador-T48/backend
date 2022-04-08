7ea99c8b950bb263435b21d33f90701c14a75584
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-04-08 11:32:48 -0300

    alterando List para Optional

--- redesocial/src/main/java/org/generation/redesocial/repository/UsuarioRepository.java
+++ redesocial/src/main/java/org/generation/redesocial/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package org.generation.redesocial.repository;
 
-import java.util.List;
+
+
+import java.util.Optional;
 
 import org.generation.redesocial.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	public Optional<Usuario> findByUsuario(String usuario);
 }
